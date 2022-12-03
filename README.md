e97072f0952721f985a7d1edd0476f5523057019
Divyanshu <kk5939875@gmail.com>
2022-12-03 13:45:09 +0530

    Some changes

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Let see how it work , click on link below 👇🏻 and enjoy it
 
  
   
- ## ![ezgif com-gif-maker](https://user-images.githubusercontent.com/68281476/148099287-157fad00-96f3-4e0a-959b-7e0b50294fb7.png) Live Demo [Live CoderTab](https://codertab.netlify.app/)![ezgif com-gif-maker (1)](https://user-images.githubusercontent.com/68281476/148098275-41e18429-ef53-4a73-bb42-153fb68d95f9.gif)
+ ## ![ezgif com-gif-maker](https://user-images.githubusercontent.com/68281476/148099287-157fad00-96f3-4e0a-959b-7e0b50294fb7.png) Live Demo [CoderLive](https://codertab.netlify.app/)![ezgif com-gif-maker (1)](https://user-images.githubusercontent.com/68281476/148098275-41e18429-ef53-4a73-bb42-153fb68d95f9.gif)
 
  
---
+++ src/components/CodeEditorWindow.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+import Editor from "@monaco-editor/react";
+
+
+const CodeEditorWindow = ({ onChange, language, code, theme, isFullScreen, Fontoptions }) => {
+
+
+    const [value, setValue] = useState(code || "")
+
+    React.useEffect(() => {
+        setValue(code)
+    }, [code])
+    const handleEditorChange = (value) => {
+        setValue(value);
+        onChange("code", value);
+    };
+    return (
+        <div className="overlay mt-1 overflow-hidden w-full h-full shadow-4xl"
+
+
+        >
+            <Editor
+                options={Fontoptions}
+                height={"100%"}
+                width={`100%`}
+                language={language || "javascript"}
+                value={value}
+                theme={theme}
+                autoIndent={true}
+                defaultValue="// some comment"
+                onChange={handleEditorChange}
+            />
+        </div>
+    )
+}
+
+export default CodeEditorWindow
